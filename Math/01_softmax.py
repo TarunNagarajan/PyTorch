68518b9b6749c8fb221b0573923b436d73fe96c5
Tarun Nagarajan <tarun.greenville@gmail.com>
2025-07-07 20:43:00 +0530

    Update 01_softmax.py

--- Math/01_softmax.py
+++ Math/01_softmax.py
@@ -3,6 +3,6 @@ import torch.nn.functional as F
 
 vector = torch.randn(10, dtype = torch.float32)
 print("Input vector: ", vector)
-output = F.softmax(vector, dim = -1)
+output = F.softmax(vector, dim = -1) # dim = -1 basically means that we are applying the operation among the last dimension
 
 print("Output vector: ", output)
